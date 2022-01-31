77444f99f80e2c775659418b2227c0968f554803
divyansh singh <52194749+devsingh1234@users.noreply.github.com>
2022-01-31 11:22:49 +0530

    django

--- djangouniversity/views.py
+++ djangouniversity/views.py
@@ -1,8 +1,12 @@
+from multiprocessing import context
+from django import forms
 from django.conf.urls import url
 from django.contrib import admin
 from django.http.response import HttpResponse
 from .import views
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import inlineformset_factory
 
 
 def homepage(request):
@@ -15,5 +19,15 @@ def about(request):
     #return HttpResponse('about')
 
 def about(request):
-    return render(request,'login.html')
+    form = UserCreationForm()
+    context = {'form' : form}
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    return render(request,'signup.html',context)
     #return HttpResponse('login')    
+
